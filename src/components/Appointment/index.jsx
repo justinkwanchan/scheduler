b9b7ca8a284ca02c0f3c4691dafaf90d3d1af41b
justinkwanchan <justinkwanchan@gmail.com>
2021-01-19 19:03:09 -0500

    Add Header and Empty components

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,3 +1,6 @@
 import React from 'react';
+import 'components/Appointment/styles.scss'
 
-return <article className="appointment"></article>;
+export default function Appointment(props) {
+  return <article className="appointment"></article>;
+};
