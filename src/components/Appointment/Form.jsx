cb2d890d0474cabacd30cd8ffe2f236663228724
justinkwanchan <justinkwanchan@gmail.com>
2021-01-25 17:29:25 -0500

    Implement Jest tests for Form and save

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -6,6 +6,7 @@ import Button from 'components/Button';
 export default function Form(props) {
   const [name, setName] = useState(props.name || '');
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState('');
 
   const reset = () => {
     setName('');
@@ -17,22 +18,30 @@ export default function Form(props) {
     props.onCancel();
   };
 
+  function validate() {
+    if (name === '') {
+      setError('Student name cannot be blank');
+      return;
+    }
+
+    props.onSave(name, interviewer);
+  }
+
   return (
     <main className='appointment__card appointment__card--create'>
       <section className='appointment__card-left'>
         <form autoComplete='off' onSubmit={event => event.preventDefault()}>
           <input
+            data-testid='student-name-input'
             className='appointment__create-input text--semi-bold'
             name='name'
             type='text'
             placeholder='Enter Student Name'
             value={name}
             onChange={event => setName(event.target.value)}
-            /*
-            This must be a controlled component
-          */
           />
         </form>
+        <section className='appointment__validation'>{error}</section>
         <InterviewerList
           interviewers={props.interviewers}
           value={interviewer}
@@ -44,7 +53,7 @@ export default function Form(props) {
           <Button danger onClick={cancel}>
             Cancel
           </Button>
-          <Button confirm onClick={() => props.onSave(name, interviewer)}>
+          <Button confirm onClick={() => validate()}>
             Save
           </Button>
         </section>
